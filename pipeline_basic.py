0096b9f7b7c15cf387484699651227ab1f1e49db
epadexp <epadexp@saosl.com>
2025-02-05 09:25:58 +0000

    Update pipeline_basic.py

--- pipeline_basic.py
+++ pipeline_basic.py
@@ -1,6 +1,22 @@
 import os
+import logging
+
+
+from pydantic import BaseModel
+from typing import List
+
+
+logging.basicConfig(level=logging.DEBUG)
 
 class Pipeline:
 
-    def print_hola():
-        return "Hola"
+    class Valves(BaseModel):
+        DB_HOST: str
+        DB_PORT: str
+        DB_USER: str
+        DB_PASSWORD: str
+        DB_DATABASE: str
+        DB_TABLES: List[str]
+        VLLM_HOST: str
+        OPENAI_API_KEY: str
+        TEXT_TO_SQL_MODEL: str
