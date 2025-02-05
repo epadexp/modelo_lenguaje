3a7bee7d46a7a9aa86146ae259b383d98462da3f
epadexp <epadexp@saosl.com>
2025-02-05 09:32:12 +0000

    Update pipeline_basic.py

--- pipeline_basic.py
+++ pipeline_basic.py
@@ -20,3 +20,23 @@ class Pipeline:
         VLLM_HOST: str
         OPENAI_API_KEY: str
         TEXT_TO_SQL_MODEL: str
+
+    def __init__(self):
+        self.name = "01 Database RAG Pipeline vLLM llama"
+        self.conn = None
+        self.nlsql_response = ""
+
+        self.valves = self.Valves(
+            **{
+                "pipelines": ["*"],
+                "DB_HOST": os.getenv("PG_HOST", "http://10.30.164.243"),
+                "DB_PORT": os.getenv("PG_PORT", '5432'),
+                "DB_USER": os.getenv("PG_USER", "admin"),
+                "DB_PASSWORD": os.getenv("PG_PASSWORD", "tester123"),
+                "DB_DATABASE": os.getenv("PG_DB", "postgres"),
+                "DB_TABLES": ["movies"],
+                "VLLM_HOST": os.getenv("VLLM_HOST", "http://10.1.152.55:8012/v1"),
+                "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY", 'abc-123'),
+                "TEXT_TO_SQL_MODEL": "NousResearch/Meta-Llama-3-8B-Instruct"
+            }
+        )
