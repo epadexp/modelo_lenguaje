67afd6dccbb7cb4f68189a03f504bb77c6acaadd
epadexp <epadexp@saosl.com>
2025-02-07 14:13:51 +0000

    Update 06_pipeline_consulta_bbdd.py

--- 06_pipeline_consulta_bbdd.py
+++ 06_pipeline_consulta_bbdd.py
@@ -97,6 +97,7 @@ class Pipeline:
         5. No devuelvas texto descriptivo, solo la consulta SQL.
         6. Si el usuario especifica ine, tienes que buscar entre las tablas que empiezan por ine
         7. Si el usuario especifica istac, tienes que buscar entre las tablas que empiezan por istac
+        8. Si el usuario especifica un año, tienes que buscar en la columna anios
 
         **Ejemplos de entrada y salida:**
 
